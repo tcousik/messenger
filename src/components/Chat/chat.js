710dc3aff9071de53ec195bb48cec426c9ec76fa
Tejas Cousik <tcousik0@gmail.com>
2020-08-07 14:58:53 -0600

    text align

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -15,8 +15,8 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  // const ENDPOINT = "https://tc-chat-app.herokuapp.com/";
-  const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "https://tc-chat-app.herokuapp.com/";
+  // const ENDPOINT = "localhost:5000";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
